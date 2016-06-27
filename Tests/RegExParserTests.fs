f3693fd2d8f3e33d35b28ada24f46803494f5807
Martin Jensen <ingvar@netaben.dk>
2016-06-27 20:45:09 +0200

    Removing dead test code

--- Tests/RegExParserTests.fs
+++ Tests/RegExParserTests.fs
@@ -4,44 +4,6 @@ open NUnit.Framework
 open RegularExpression
 open RegularExpression.Parser
 
-
-[<TestFixture>]
-type RegExParser() = 
- static member validData =
-        [
-            // Escape tests
-
-            // Concatination tests
-        //    TestCaseData("a" |> List.ofSeq).Returns(Char('a')).SetName("a");
-        //    TestCaseData("ab" |> List.ofSeq).Returns(Concatination(Char('a'), Char('b'))).SetName("ab");
-        //    TestCaseData("abc" |> List.ofSeq).Returns(Concatination(Char('a'), Concatination(Char('b'), Char('c')))).SetName("abc");
-
-            // Group tests
-        //    TestCaseData("(a)" |> List.ofSeq).Returns(Group(Char('a'))).SetName("(a)");
-         //   TestCaseData("(ab)" |> List.ofSeq).Returns(Group(Concatination(Char('a'), Char('b')))).SetName("(ab)");
-         //   TestCaseData("(a(b))" |> List.ofSeq).Returns(Group(Concatination(Char('a'), Group(Char('b'))))).SetName("(a(b))")
-         //   TestCaseData("(a)(b)" |> List.ofSeq).Returns(Concatination(Group(Char('a')), Group(Char('b')))).SetName("(a)(b)");
-
-            // Choise tests
-        //    TestCaseData("a|b" |> List.ofSeq).Returns(Choise(Char('a'), Char('b'))).SetName("a|b");
-       //     TestCaseData("ac|bd" |> List.ofSeq).Returns(Choise(Concatination(Char('a'), Char('c')), Concatination(Char('b'), Char('d')))).SetName("ac|bd");
-       //     TestCaseData("a|b|c" |> List.ofSeq).Returns(Choise(Char('a'), Choise(Char('b'), Char('c')))).SetName("a|b|c");
-
-            // Group+Choise tests
-        //    TestCaseData("(a)|(b)" |> List.ofSeq).Returns(Choise(Group(Char('a')), Group(Char('b')))).SetName("(a)|(b)");
-        //    TestCaseData("(a|b)" |> List.ofSeq).Returns(Group(Choise(Char('a'), Char('b')))).SetName("(a|b)");
-
-            // Star tests
-
-            // Plus tests
-        ]
-
-   // [<Test>]
-   // [<TestCaseSource(typedefof<RegExParser>, "validData")>]
-   // member x.TestCase data =
-     //   parse data
-
-
 [<TestFixture>]
 type RegExParser_parseSymbol() = 
     static member validData =
@@ -58,12 +20,12 @@ type RegExParser_parseSymbol() =
 
     [<Test>]
     member x.EmptyStreamThrows() =
-        Assert.Throws<System.Exception>(fun () -> parseSymbol [] |> ignore) |> ignore
+        Assert.Throws<Exception>(fun () -> parseSymbol [] |> ignore) |> ignore
 
 
     [<Test>]
     member x.UnsupportedSymbolThrows() =
-        Assert.Throws<System.Exception>(fun () -> parseSymbol ['å'] |> ignore) |> ignore
+        Assert.Throws<Exception>(fun () -> parseSymbol ['å'] |> ignore) |> ignore
 
 
 [<TestFixture>]
