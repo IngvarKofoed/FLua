eeeb6fff9bdb0be9bfec5ab5cf590759fa401159
Martin Jensen <ingvar@netaben.dk>
2016-06-28 20:16:59 +0200

    First take on an evaluator, that only support DFAs. Next would be to make a parsed regex to NFA and then the NFA to DFA transformations

--- ParserConsole/Program.fs
+++ ParserConsole/Program.fs
@@ -1,12 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
-open RegularExpression
-open RegularExpression.Parser
+open RegularExpression.Evaluator
 
 [<EntryPoint>]
 let main argv = 
 
- 
+    let graph = [{ value = Char 'a'; fromNode = 0; toNode = 1 } ]
+    
+    let res = evaluateRegEx 0 graph (List.ofSeq "a")
 
    
     0 // return an integer exit code
