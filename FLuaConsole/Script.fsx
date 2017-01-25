a10f1b4877be37ee3e76bd9c8ccc31b0c5aa49ec
Martin Jensen <ingvar@netaben.dk>
2017-01-25 20:31:50 +0100

    Strategy change

---
+++ FLuaConsole/Script.fsx
@@ -0,0 +1,108 @@
+﻿// Very strongly inspired by the project: http://www.quanttec.com/fparsec/
+
+
+type Stream = char list
+
+
+type Result<'a> =
+    | Success of result: 'a * stream: Stream
+    | Failure
+
+
+type Parser<'a> = Parser of parser: (Stream -> Result<'a>)
+
+
+let runParser parser stream =
+    let Parser (parser = parserFunc) = parser
+    parserFunc stream
+
+let returnParser x =
+    let inner stream =
+        Success(x, stream)
+    Parser inner
+
+let orParser parser1 parser2 = 
+    let inner stream = 
+        let result1 = runParser parser1 stream
+        match result1 with
+        | Success _ -> result1
+        | Failure ->
+            let result2 = runParser parser2 stream
+            result2
+    Parser inner
+
+
+let ( <|> ) = orParser
+
+
+let andParser parser1 parser2 =
+    let inner stream =
+        let result1 = runParser parser1 stream
+        match result1 with 
+        | Success (res1, rest1) ->
+            let result2 = runParser parser2 rest1
+            match result2 with
+            | Success (res2, rest2) -> Success((res1, res2), rest2)
+            | Failure -> Failure
+        | Failure -> Failure
+    Parser inner
+
+ 
+let ( .>>. ) = andParser
+
+
+
+let rec choise parsers =
+    let inner stream =
+        match parsers with
+        | x::xs -> 
+            let result = runParser x stream
+            match result with
+            | Success _ -> result
+            | Failure -> runParser (choise xs) stream
+        | _ -> Failure
+    Parser inner
+
+
+let bindParser parser func =
+    let inner stream =
+        let result = runParser parser stream
+        match result with 
+        | Success (res, rest) -> 
+            let fParser = func res
+            runParser fParser rest
+        | Failure -> Failure
+    Parser inner  
+
+let ( >>= ) = bindParser
+
+
+let mapParser parser func =
+    let inner stream =
+        let result = runParser parser stream
+        match result with 
+        | Success (res, rest) -> Success(func res, rest)
+        | Failure -> Failure
+    Parser inner
+
+let ( |>> ) = mapParser
+
+let rec many parser =
+    (parser >>= fun res -> many parser |>> fun r -> res::r)
+    <|> returnParser []
+        
+   
+let charParser char =
+    let inner stream =
+        match stream with
+        | x::xs when char = x -> Success(char, xs)
+        | _ -> Failure
+    Parser inner
+
+  
+let myParser = many (charParser 'a') .>>. charParser 'b'
+
+let result = runParser myParser (List.ofSeq "aaaab")
+
+
+
--- README.md
+++ README.md
@@ -2,9 +2,6 @@ This is me learning F# by implementing a parser for some yet unspecified languag
 
 The goal is to get to know F# really good and not to produce a fancy, pretty or production ready parser...
 
-I started out trying to implement a parser for regular expressions by handcoding everything and only using 1 look forward. 
-Although this worked it got rather complex and was not very smart implemented. After some time i discovered FParsec http://www.quanttec.com/fparsec/. 
-This gave new ideas to how to implement my lua parser. To acomplis my goal of learning F# I will NOT use the FParsec library, although it looks really cool! 
-I will by looking at the FParsec documentation make my own subset implementation of FParsec and use that for my lua parser. 
-I will keep the the CharStream as simple as possible and not use RegEx from the .NET library.
+I changed my strategy to be inspired by the FParsec project and implement a small subset of FParsec by using the concept and idea in FParsec and looking at the function types.
+I will use this small reimplementation of FParsec to make a lua parser. 
 
